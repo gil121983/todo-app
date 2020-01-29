b00b9a85774bac778d14315f2865639c88880084
gil121983 <gilstolar@icloud.com>
2020-01-29 22:30:34 +0200

    sweet alerts works

--- js/dialog.js
+++ js/dialog.js
@@ -1,120 +1,273 @@
-var dict = {
-    ADD: {
-      title: 'Add ToDo',
-      content: getAddToDoForm
-    },
-    DELETE: {
-      title: 'Delete ToDo',
-      content: getDeleteToDoForm
-    },
-    EDIT: {
-      title: 'Edit Todo',
-      content: getEditToDoForm
-    },
-  }
 
-  function testSweetAlert(){
-    Swal.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      icon: 'warning',
+var dict = {
+  ADD: {
+    title: 'Add ToDo',
+    content: getAddToDoForm
+  },
+  DELETE: {
+    title: 'Delete ToDo',
+    content: getDeleteToDoForm
+  },
+  EDIT: {
+    title: 'Edit Todo',
+    content: getEditToDoForm
+  },
+  VIEW: {
+    title: 'View ToDo',
+    content: viewTodo
+  }
+}
+
+function openDialog(type, todo) {
+  var dialogElement = document.querySelector('.dialog');
+  var data = dict[type];
+  var content = data.content(todo);
+}
+
+function viewTodo(todo){
+  Swal.fire({
+    title: todo.title,
+    text: todo.description + " - " + todo.status +" - " + todo.priority,
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Edit'
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire("How do I make this work?")
+    }
+  })
+}
+
+function getAddToDoForm() {
+
+  (async () => {
+    var newTodoObj = {};
+    const { value: priority } = await Swal.fire({
+      title: 'Set Priority',
+      input: 'select',
+      inputOptions: {
+        moderate: 'Moderate',
+        high: 'High',
+        low: 'Low'
+      },
+      inputPlaceholder: 'Select a priority',
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-      if (result.value) {
-        
-        
-        Swal.fire(
-          'Deleted!',
-          'Your file has been deleted.',
-          'success'
-        )
+      inputValidator: (value) => {
+        return new Promise((resolve) => {
+          if (value === 'low' || value === 'high' || value === 'moderate') {
+            resolve()
+            newTodoObj.priority = value;
+          } else {
+            resolve("you must pick priority!");
+          }
+        })
       }
     })
-  }
+    if (priority) {
+      Swal.fire(`You selected: ${priority}`)
 
-  function openDialog(type, todo) {
-    var dialogElement = document.querySelector('.dialog');
-    dialogElement.setAttribute('style', 'display: block');
-  
-    var data = dict[type];
-  
-    var titleElement = dialogElement.querySelector('.title');
-    titleElement.innerHTML = data.title;
-  
-    var contentElement = dialogElement.querySelector('.content');
-    contentElement.innerHTML = '';
-    var content = data.content(todo);
-    contentElement.appendChild(content);
-  }
-  
-  function getAddToDoForm() {
-    var formElement = document.createElement('form');
-  
-    var descInputElement = document.createElement('input');
-    descInputElement.placeholder = 'Description:';
-    formElement.appendChild(descInputElement);
-  
-    var submitButton = document.createElement('button');
-    submitButton.innerHTML = 'submit'
-    formElement.appendChild(submitButton);
-  
-    formElement.onsubmit = function (e) {
-      e.preventDefault();
-      toDoList.push({description: descInputElement.value });
-      closeDialog();
-      render();
-    }
-  
-    return formElement
-  }
-  
-  function getEditToDoForm(todo) {
-    var container = document.createElement('form');
-  
-    var descInputElement = document.createElement('input');
-    descInputElement.value = todo.description;
-    container.append(descInputElement);
-  
-  
-    var saveButton = document.createElement('button');
-    saveButton.innerHTML = 'Save'
-    container.appendChild(saveButton);
-  
-    container.onsubmit = function (e) {
-      e.preventDefault();
-      let position = toDoList.indexOf(todo)
-      toDoList.splice(position, 1, {
-        description: descInputElement.value,
+      const { value: title } = await Swal.fire({
+        title: 'Enter a title',
+        input: 'text',
+        showCancelButton: true,
+        inputValidator: (value) => {
+          if (!value) {
+            return 'You need to write something!'
+          }
+        }
       })
-      console.log("Changes has been saved");
-      closeDialog();
-      render();
-    }
-    return container;
-  }
-  
-  
-  function getDeleteToDoForm(todo) {
 
-  
-    element.onsubmit = function (e) {
-      e.preventDefault();
+      if (title) {
+        Swal.fire(`title is ${title}`)
+        newTodoObj.title = title;
+        
+        const { value: text } = await Swal.fire({
+          input: 'textarea',
+          inputPlaceholder: 'Describe your task...',
+          inputAttributes: {
+            'aria-label': 'Type your message here'
+          },
+          showCancelButton: true
+        })
+        
+        if (text) {
+          Swal.fire(text)
+          newTodoObj.description = text;
+          newTodoObj.status = "In Progress"
+          toDoList.push(newTodoObj);
+          init();
+
+          const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            onOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+          
+          Toast.fire({
+            icon: 'success',
+            title: 'saved successfully'
+          })
+        }
+
+      }
+    }
+  })()
+}
+
+function getEditToDoForm(todo) {
+  (async () => {
+    var currentTodoObj = todo;
+    const { value: priority } = await Swal.fire({
+      title: 'Set Priority',
+      input: 'select',
+      inputValue: todo.priority,
+      inputOptions: {
+        moderate: 'Moderate',
+        high: 'High',
+        low: 'Low'
+      },
+      inputPlaceholder: 'Select a priority',
+      showCancelButton: true,
+      inputValidator: (value) => {
+        return new Promise((resolve) => {
+          if (value === 'low' || value === 'high' || value === 'moderate') {
+            resolve()
+            currentTodoObj.priority = value;
+          } else {
+            resolve("you must pick priority!");
+          }
+        })
+      }
+    })
+    if (priority) {
+      Swal.fire(`You selected: ${priority}`)
+
+      const { value: title } = await Swal.fire({
+        title: 'Enter a title',
+        input: 'text',
+        inputValue: todo.title,
+        showCancelButton: true,
+        inputValidator: (value) => {
+          if (!value) {
+            return 'You need to write something!'
+          }
+        }
+      })
+
+      if (title) {
+        Swal.fire(`title is ${title}`)
+        currentTodoObj.title = title;
+        
+        const { value: text } = await Swal.fire({
+          input: 'textarea',
+          inputValue: todo.text,
+          inputAttributes: {
+            'aria-label': 'Type your message here'
+          },
+          showCancelButton: true
+        })
+        
+        if (text) {
+          Swal.fire(text)
+          currentTodoObj.description = text;
+          
+          const inputOptions = new Promise((resolve) => {
+            setTimeout(() => {
+              resolve({
+                "In Progress": "In Progress",
+                "In Review" : "In Review",
+                "Done": "Done"
+              })
+            }, 1000)
+          })
+          
+          const { value: status } = await Swal.fire({
+            title: 'Select Status',
+            input: 'radio',
+            inputValue: todo.status,
+            inputOptions: inputOptions,
+            inputValidator: (value) => {
+              if (!value) {
+                return 'You need to choose something!'
+              }
+            }
+          })
+          
+          if (status) {
+            Swal.fire({ html: `You selected: ${status}` })
+            currentTodoObj.status = status;
+          }
+
+          init();
+
+          const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            onOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+          console.log(currentTodoObj);
+          
+          Toast.fire({
+            icon: 'success',
+            title: 'saved changes'
+          })
+        }
+
+      }
+    }
+  })()
+}
+
+
+function getDeleteToDoForm(todo) {
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+    if (result.value) {
       let position = toDoList.indexOf(todo)
       toDoList.splice(position, 1)
-      closeDialog();
       render();
-  
+    
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        onOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+        icon: 'success',
+        title: 'Task Deleted'
+      })
     }
-    return element;
-  }
-  
-  function closeDialog() {
-    var dialogElement = document.querySelector('.dialog');
-    dialogElement.setAttribute('style', 'display: none');
-  }
+  })
 
 
-  
+}
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,45 +1,51 @@
 init();
 
 function init() {
-    render();
+  render();
+}
+
+function onView(todo, e) {
+  openDialog('VIEW', todo);
 }
 
 function onEdit(todo, e) {
-    openDialog('EDIT', todo);
-  }
-  
-  function onDelete(todo, e) {
-    openDialog('DELETE', todo);
-  }
+  openDialog('EDIT', todo);
+}
+
+function onDelete(todo, e) {
+  openDialog('DELETE', todo);
+}
 
 function render() {
-    var listElement = document.querySelector('.todo-list');
-    listElement.innerHTML = '';
-    for (let index = 0; index < toDoList.length; index++) {
-        var todo = toDoList[index];
+  var listElement = document.querySelector('.todo-list');
+  listElement.innerHTML = '';
+  for (let index = 0; index < toDoList.length; index++) {
+    var todo = toDoList[index];
 
-        var todoElement = document.createElement('div');
-        todoElement.className = 'todo';
-        todoElement.innerHTML = todo.description;
-        
-        var priorityElement = document.createElement('div');
-        priorityElement.className = 'todo';
-        priorityElement.innerHTML = todo.priority;
+    var todoElement = document.createElement('div');
+    todoElement.className = 'todo';
+    todoElement.innerHTML = " status: " + todo.status + "  priority: " + todo.priority + " - " + todo.title ;
 
-        var editButton = document.createElement('button');
-        editButton.className = 'edit-button';
-        editButton.innerHTML = 'EDIT';
-        editButton.onclick = onEdit.bind(event, todo);
+    var viewButton = document.createElement('button');
+    viewButton.className = 'view-button';
+    viewButton.innerHTML = 'VIEW';
+    viewButton.onclick = onView.bind(event, todo);
 
-        var deleteButton = document.createElement('button');
-        deleteButton.className = 'delete-button';
-        deleteButton.innerHTML = 'DELETE';
-        deleteButton.onclick = onDelete.bind(event, todo);
+    var editButton = document.createElement('button');
+    editButton.className = 'edit-button';
+    editButton.innerHTML = 'EDIT';
+    editButton.onclick = onEdit.bind(event, todo);
 
-        todoElement.appendChild(editButton);
-        todoElement.appendChild(deleteButton);
+    var deleteButton = document.createElement('button');
+    deleteButton.className = 'delete-button';
+    deleteButton.innerHTML = 'DELETE';
+    deleteButton.onclick = onDelete.bind(event, todo);
 
-        listElement.appendChild(todoElement);
+    todoElement.appendChild(viewButton);
+    todoElement.appendChild(editButton);
+    todoElement.appendChild(deleteButton);
 
-    }
+    listElement.appendChild(todoElement);
+
+  }
 }
